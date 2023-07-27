d8b4ccf8c30d2bc098636ec74868c0059f558cb8
Pius Udoma <udomahpius5@gmail.com>
2023-07-27 19:09:20 +0100

    init

--- public_html/assets/js/indexScript.js
+++ public_html/assets/js/indexScript.js
@@ -50,7 +50,68 @@ $(window).on("load", function() {
                 duration: 750,
                 easing: "linear",
                 queue: !1
+             
             }
         }), !1
     })
 })
+
+// setting toggle backgroung color
+const setNavbarToggleBackgroundColor = () => {
+    let navBarToggleButtonElement = document.querySelector('.setToggleColor');
+    let navBarToggleClassElement = document.querySelector('.bgToggleColor');
+    navBarToggleButtonElement.addEventListener('click', () => {
+        let toggle = false
+        if (!toggle) {
+            navBarToggleClassElement.classList.add('toggleBgColor');
+            toggle = true;
+        } else {
+            navBarToggleClassElement.classList.remove('toggleBgColor');
+            toggle = false;
+        }
+    })
+}
+setNavbarToggleBackgroundColor()
+
+
+// setting onscroll navbar backgroundColor
+
+const windowScrollBackroundColor = () => {
+    let navElement = document.querySelector('.scrollBackgroundBg');
+    let btnScrollBg = document.querySelector('.btn-scroll')
+    window.addEventListener('scroll', changeBgColor)
+    function changeBgColor() {
+            if (window.scrollY >= 20) {
+                navElement.classList.add('xl-display-bg');
+                btnScrollBg.classList.add('btn-primary-scroll');
+
+            } else {
+                navElement.classList.remove('xl-display-bg');
+                btnScrollBg.classList.remove('btn-primary-scroll');
+            }
+    }
+
+}
+windowScrollBackroundColor()
+
+//changing text content
+
+            let textElement_1 = document.querySelector('.changeContent_1');
+            let textElement_2 = document.querySelector('.changeContent_2');
+            let textElement_3 = document.querySelector('.changeContent_3');
+            let textElement_4 = document.querySelector('.changeContent_4');
+
+
+            let textChange = document.querySelector('.change-text');
+            textElement_1.addEventListener('click', () => {
+                textChange.innerHTML = 'Academic and Professional Qualifications'
+            })
+            textElement_2.addEventListener('click', () => {
+                textChange.innerHTML = 'WORKING EXPERIENCE'
+            })
+            textElement_3.addEventListener('click', () => {
+                textChange.innerHTML = 'COURSES TAUGHT AT VARIOUS TIMES'
+            })
+            textElement_4.addEventListener('click', () => {
+                textChange.innerHTML = 'OTHER RESPONSIBILITIES OUTSIDE TEACHING'
+            })
